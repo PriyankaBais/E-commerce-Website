4923d65b1cee2aec2c18716ef213c8d657af5a47
Priyanka Bais <priyanka0601bais@gmail.com>
2025-06-07 12:41:54 +0530

    E-commerce  Website

---
+++ script.js
@@ -0,0 +1,5 @@
+    //categories sub section 
+function toggleDropdown() {
+            const dropdown = document.querySelector('.dropdown');
+            dropdown.classList.toggle('open');
+        }
